642e483960f1c9b079462caf8734d2bec76ee651
basim ghouri <ghouri.geeks@gmail.com>
2023-01-20 23:48:17 +0500

    debug code

---
+++ App.js
@@ -0,0 +1,75 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList,} from 'react-native';
+
+export default function App() {
+
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalText]);
+  };
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} placeholder='Your course goal!' onChangeText={goalInputHandler} />
+        <Button title='Add Goal' onPress={addGoalHandler}/>
+      </View>
+     <View style={styles.goalsContainer}>
+      <FlatList data={courseGoals} renderItem={(itemData) => {
+        return (
+        <View style={styles.goalItem}>
+          <Text style={styles.goalText}>{itemData.item}</Text>
+        </View>
+        );
+      }} 
+      alwaysBounceHorizontal={false}/>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  appContainer:{
+    flex:1,
+    padding:60,
+    paddingHorizontal:16
+  },
+  inputContainer:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'#cccccc'
+  },
+  TextInput:{
+    borderWidth:1,
+    borderColor: '#cccccc',
+    width:'70%',
+    marginRight:8,
+    padding:8
+  },
+  goalsContainer:{
+    flex:5
+  },
+  goalItem:{
+    margin:8,
+    padding:8,
+    borderRadius:6,
+    backgroundColor:'#5e0acc',
+    color:'#fff'
+  },
+  goalText:{
+    color:'#fff'
+  }
+
+});
